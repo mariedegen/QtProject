55a33c7d4505d7dba2debde83707d3c70804382f
Stéphane <boluge@gmail.com>
2018-03-06 17:53:54 +0100

    voilaaaaaa

--- model/type.h
+++ model/type.h
@@ -1,11 +1,30 @@
 #ifndef TYPE_H
 #define TYPE_H
-
+#include <QString>
 
 class Type
 {
+private:
+    /*
+     *   Id : primary key, integer
+     *   Label : QString
+     */
+
+    int id;
+    QString label;
+
 public:
+    /* Constructos & Destructor */
     Type();
+    Type(const QString lbl);
+    ~Type();
+
+    /* Methods */
+    int getId() const;
+    void setId(int value);
+
+    QString getLabel() const;
+    void setLabel(const QString &value);
 };
 
 #endif // TYPE_H
